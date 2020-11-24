fb9f10ca0077b6b3b49b7c5c07fdc01670a9112f
Eric Syrén <eric.syren@tactel.se>
2020-11-24 10:53:42 +0100

    Correct test cases

--- kronos-java/src/test/java/com/lyft/kronos/internal/ntp/SntpServiceTest.kt
+++ kronos-java/src/test/java/com/lyft/kronos/internal/ntp/SntpServiceTest.kt
@@ -18,17 +18,18 @@ class SntpServiceTest {
     private val sntpSyncListener = mock<SyncListener>()
     private val mockResponse = mock<SntpClient.Response>()
     private val ntpHosts = listOf("2.fake.pool.ntp.org", "1.fake.pool.ntp.org", "0.fake.pool.ntp.org")
+    private val ntpPort = 123
 
     init {
         whenever(deviceClock.getElapsedTimeMs()).then({ System.currentTimeMillis() })
-        sntpService = SntpServiceImpl(sntpClient, deviceClock, responseCache, sntpSyncListener, ntpHosts)
+        sntpService = SntpServiceImpl(sntpClient, deviceClock, responseCache, sntpSyncListener, ntpHosts, ntpPort)
     }
 
     @Test
     fun testRecoverFromNetworkError() {
 
         val mockError = mock<IOException>()
-        whenever(sntpClient.requestTime(any(), any())).thenThrow(mockError)
+        whenever(sntpClient.requestTime(any(), any(), any())).thenThrow(mockError)
         assertThat(sntpService.sync()).isFalse()
 
         verify(sntpSyncListener, times(3)).onStartSync(any())
@@ -36,8 +37,8 @@ class SntpServiceTest {
         verify(sntpSyncListener, times(1)).onError("1.fake.pool.ntp.org", mockError)
         verify(sntpSyncListener, times(1)).onError("0.fake.pool.ntp.org", mockError)
 
-        whenever(sntpClient.requestTime(any(), any())).thenReturn(mockResponse)
-        assertThat(sntpService.sync()).isTrue
+        whenever(sntpClient.requestTime(any(), any(), any())).thenReturn(mockResponse)
+        assertThat(sntpService.sync()).isTrue()
 
         verify(sntpSyncListener, times(2)).onStartSync("2.fake.pool.ntp.org")
         verify(sntpSyncListener, times(1)).onSuccess(any(), any())
@@ -46,9 +47,9 @@ class SntpServiceTest {
     @Test
     fun testOneOfThreeServerHasError() {
         val mockError = mock<IOException>()
-        whenever(sntpClient.requestTime("2.fake.pool.ntp.org", TIMEOUT_MS)).thenThrow(mockError)
-        whenever(sntpClient.requestTime("1.fake.pool.ntp.org", TIMEOUT_MS)).thenReturn(mockResponse)
-        assertThat(sntpService.sync()).isTrue
+        whenever(sntpClient.requestTime("2.fake.pool.ntp.org", ntpPort, TIMEOUT_MS)).thenThrow(mockError)
+        whenever(sntpClient.requestTime("1.fake.pool.ntp.org", ntpPort, TIMEOUT_MS)).thenReturn(mockResponse)
+        assertThat(sntpService.sync()).isTrue()
 
         verify(sntpSyncListener, times(1)).onStartSync("2.fake.pool.ntp.org")
         verify(sntpSyncListener, times(1)).onError("2.fake.pool.ntp.org", mockError)
@@ -64,9 +65,9 @@ class SntpServiceTest {
                 .thenReturn(now + MAX_NTP_RESPONSE_TIME_MS + 1) //first response time
                 .thenReturn(now + MAX_NTP_RESPONSE_TIME_MS + 10) //second request time
                 .thenReturn(now + MAX_NTP_RESPONSE_TIME_MS + 50) //second response time
-        whenever(sntpClient.requestTime("2.fake.pool.ntp.org", TIMEOUT_MS)).thenReturn(mockResponse)
-        whenever(sntpClient.requestTime("1.fake.pool.ntp.org", TIMEOUT_MS)).thenReturn(mockResponse)
-        assertThat(sntpService.sync()).isTrue
+        whenever(sntpClient.requestTime("2.fake.pool.ntp.org", ntpPort, TIMEOUT_MS)).thenReturn(mockResponse)
+        whenever(sntpClient.requestTime("1.fake.pool.ntp.org", ntpPort, TIMEOUT_MS)).thenReturn(mockResponse)
+        assertThat(sntpService.sync()).isTrue()
 
         verify(sntpSyncListener, times(1)).onStartSync("2.fake.pool.ntp.org")
         verify(sntpSyncListener, times(1)).onError(eq("2.fake.pool.ntp.org"), any<NTPSyncException>())
@@ -77,7 +78,7 @@ class SntpServiceTest {
     @Test
     fun shouldReturnTimeUnavailableAfterShutdown() {
         sntpService.shutdown()
-        verify(sntpClient, never()).requestTime(any(), any())
+        verify(sntpClient, never()).requestTime(any(), any(), any())
         assertThatExceptionOfType(IllegalStateException::class.java).isThrownBy { sntpService.sync() }
         assertThatExceptionOfType(IllegalStateException::class.java).isThrownBy { sntpService.syncInBackground() }
         assertThatExceptionOfType(IllegalStateException::class.java).isThrownBy { sntpService.currentTimeMs() }
@@ -85,11 +86,11 @@ class SntpServiceTest {
 
     @Test
     fun `throw error if response has negative time value`() {
-        whenever(sntpClient.requestTime("1.fake.pool.ntp.org", TIMEOUT_MS)).thenReturn(mockResponse)
-        whenever(sntpClient.requestTime("2.fake.pool.ntp.org", TIMEOUT_MS)).thenReturn(mockResponse)
+        whenever(sntpClient.requestTime("1.fake.pool.ntp.org", ntpPort, TIMEOUT_MS)).thenReturn(mockResponse)
+        whenever(sntpClient.requestTime("2.fake.pool.ntp.org", ntpPort, TIMEOUT_MS)).thenReturn(mockResponse)
         whenever(mockResponse.currentTimeMs).thenReturn(-1)
 
-        assertThat(sntpService.sync()).isFalse
+        assertThat(sntpService.sync()).isFalse()
 
         verify(sntpSyncListener, times(1)).onError(eq("1.fake.pool.ntp.org"), any<NTPSyncException>())
         verify(sntpSyncListener, times(1)).onError(eq("2.fake.pool.ntp.org"), any<NTPSyncException>())
@@ -97,13 +98,13 @@ class SntpServiceTest {
 
     @Test
     fun shouldClearCacheWhenSyncFailsAndGettingCurrentTime() {
-        whenever(sntpClient.requestTime("1.fake.pool.ntp.org", TIMEOUT_MS)).thenReturn(mockResponse)
-        whenever(sntpClient.requestTime("2.fake.pool.ntp.org", TIMEOUT_MS)).thenReturn(mockResponse)
+        whenever(sntpClient.requestTime("1.fake.pool.ntp.org", ntpPort, TIMEOUT_MS)).thenReturn(mockResponse)
+        whenever(sntpClient.requestTime("2.fake.pool.ntp.org", ntpPort, TIMEOUT_MS)).thenReturn(mockResponse)
         whenever(mockResponse.currentTimeMs).thenReturn(-1)
         whenever(mockResponse.isFromSameBoot).thenReturn(false)
         whenever(responseCache.get()).thenReturn(mockResponse)
 
-        assertThat(sntpService.sync()).isFalse
+        assertThat(sntpService.sync()).isFalse()
 
         sntpService.currentTime()
 
